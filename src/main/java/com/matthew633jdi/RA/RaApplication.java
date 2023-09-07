de6b800ec5d6c5f8b3d480d284e03411505b47ee
matthew633jdi <matthew633jdi@gmail.com>
2023-09-07 17:48:45 +0900

    project init

---
+++ src/main/java/com/matthew633jdi/RA/RaApplication.java
@@ -0,0 +1,13 @@
+package com.matthew633jdi.RA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matthew633jdi/RA/RaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matthew633jdi.RA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
